4351bbb4553f0b77736d3a6af162287c4ccad5f0
Esteban <esteban.escobar@gorillalogic.com>
2025-11-17 20:23:40 -0500

    Add custom tab bar component and image assets; update layout to use custom tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import CustomTabBar from '@/components/navigation/CustomTabBar'
 import { StyledTabs } from '@/components/navigation/tabs'
 import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
@@ -5,19 +6,21 @@ import React from 'react'
 
 const Layout = () => {
   return (
-    <StyledTabs headerClassName='bg-dark text-white'>
+    <StyledTabs headerClassName='bg-dark text-white' tabBar={(props) => <CustomTabBar {...props} />}>
         <Tabs.Screen name="index" options={{ headerShown: false, tabBarIcon: ({ color, size }) => (
             <Ionicons name="home-outline" color={color} size={size} />
         ) }} />
+
         <Tabs.Screen name="profile" options={{ tabBarIcon: ({ color, size }) => (
             <Ionicons name="person-outline" color={color} size={size} />
         ) }} />
+
         <Tabs.Screen name="cart" options={{ tabBarIcon: ({ color, size }) => (
             <Ionicons name="cart-outline" color={color} size={size} />
         ) }} />
-        <Tabs.Screen name="rufus" options={{ tabBarIcon: ({ color, size }) => (
-            <Ionicons name="sparkles-outline" color={color} size={size} />
-        ) }} />
+        
+        <Tabs.Screen name="rufus" />
+
         <Tabs.Screen name="more" options={{ tabBarIcon: ({ color, size }) => (
             <Ionicons name="menu-outline" color={color} size={size} />
         ) }} />
---
+++ components/navigation/CustomTabBar.tsx
@@ -0,0 +1,67 @@
+import rufusIcon from '@/assets/images/rufus.png';
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { PlatformPressable } from "@react-navigation/elements";
+import React from "react";
+import { Image, View } from "react-native";
+
+const CustomTabBar = ({
+  state,
+  descriptors,
+  navigation,
+}: BottomTabBarProps) => {
+  //   console.log('[🧪]  CustomTabBar  state', JSON.stringify(state, null, 2))
+  //   console.log('[🧪]  CustomTabBar  descriptors', JSON.stringify(descriptors, null, 2))
+  return (
+    <View className="flex-row bg-white relative border-t border-gray-200">
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name as never);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            key={index}
+
+            // accessibility props
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            
+            className="flex-1 items-center justify-center py-2 pb-safe"
+            onPress={onPress}
+            onLongPress={onLongPress}
+          >
+            {options.tabBarIcon && route.name !== 'rufus' ? (
+              options.tabBarIcon({
+                focused: isFocused,
+                size: 24,
+                color: "black",
+              })
+            ) : (
+              <Image source={rufusIcon} className="w-12 h-12" />
+            )}
+          </PlatformPressable>
+        );
+      })}
+    </View>
+  );
+};
+
+export default CustomTabBar;
